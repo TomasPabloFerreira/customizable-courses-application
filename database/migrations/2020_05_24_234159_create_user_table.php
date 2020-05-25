88bd49399e0bf0fe51248c14fdfff8da7d455c4c
TomasPabloFerreira <tomasferreira373@gmail.com>
2020-05-24 21:01:50 -0300

    chore(database): add user table migration

---
+++ database/migrations/2020_05_24_234159_create_user_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 32);
+            $table->string('surname', 32);
+            $table->string('email', 320)->unique();
+            $table->string('password', 64);
+            $table->rememberToken();
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('role');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+}
