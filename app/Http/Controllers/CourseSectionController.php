eff797b487284b1eee16f03d3d95560ba15adbf7
TomasPabloFerreira <tomasferreira373@gmail.com>
2020-06-12 20:23:18 -0300

    feat(section-crud): add update course section method

--- app/Http/Controllers/CourseSectionController.php
+++ app/Http/Controllers/CourseSectionController.php
@@ -47,9 +47,10 @@ class CourseSectionController extends Controller
 	 * Store a newly created resource in storage.
 	 *
 	 * @param  \Illuminate\Http\Request  $request
+	 * @param  int $courseId
 	 * @return \Illuminate\Http\Response
 	 */
-	public function store($courseId, Request $request)
+	public function store (Request $request, $courseId)
 	{
 		$request->validate(['title' => 'between:6,64']);
 		$section = new CourseSection([
@@ -62,5 +63,23 @@ class CourseSectionController extends Controller
 			->with('success', 'Course section has been created');
 	}
 
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $courseId
+	 * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+	public function update (Request $request, $courseId, $id)
+	{
+		$request->validate(['title' => 'between:6,64']);
+		$section = CourseSection::find($id);
+		$section->title = $request->input('title');
+
+		$section->save();
+		return redirect("/course/$courseId/section")
+			->with('success', 'Course section has been updated');
+	}
 
 }
