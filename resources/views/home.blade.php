2a5efce08003e370c81a091a607755b48b2e6fe8
TomasPabloFerreira <tomasferreira373@gmail.com>
2020-06-14 13:01:05 -0300

    feat(home-page): course listing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,12 +6,31 @@
 	<h1 class="display-3 text-white">Courses</h1>
 </b-container>
 
-<b-container fluid="lg">
-	<b-row class="justify-content-md-center">
-		<b-col cols="12">
-
-		</b-col>
-	</b-row>
+<b-container class="my-4">
+	<div>
+		<b-card-group columns>
+			@foreach ($courses as $course)
+				<b-card
+					class="mt-4"
+					title="{{ $course->title }}"
+					img-src="{{ $course->image_source }}"
+					img-alt="course_image"
+					img-top
+					style="cursor: pointer"
+					onclick="location.href='course/{{ $course->id }}'"
+				>
+					<b-card-text>
+						{{ $course-> description }}
+					</b-card-text>
+					<template v-slot:footer>
+						<small class="text-muted">
+							n lessons
+						</small>
+					</template>
+				</b-card>
+			@endforeach
+		</b-card-group>
+	</div>
 </b-container>
 
 @endsection
