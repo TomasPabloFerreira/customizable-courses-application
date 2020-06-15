a36ed277c17a7065b87e13e588769ead13942457
TomasPabloFerreira <tomasferreira373@gmail.com>
2020-06-14 23:30:56 -0300

    feat(course-detail): add course detail header

--- resources/views/course/detail.blade.php
+++ resources/views/course/detail.blade.php
@@ -2,9 +2,27 @@
 
 @section('content')
 
-<b-container class="py-4">
+<b-container fluid class="course-header bg-secondary">
+	<b-img
+		thumbnail
+		style=""
+		src="{{ $course->image_source }}"
+	>
+	</b-img>
+	<b-row class="text-row">
+		<b-col md="8" offset-md="4">
+			<h1 class="display-3 text-white">
+			{{ $course-> title }}
+			</h1>
+			<p class="description">
+				{{ $course->description }}
+			</p>
+		</b-col>
+	</b-row>
+</b-container>
 
-	@json($course)
+<b-container class="my-4">
+
 
 </b-container>
 
